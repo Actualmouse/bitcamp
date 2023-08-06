2b628635bf760e60c606c89a828c278cb0f3ea83
Actualmouse <106446042+Actualmouse@users.noreply.github.com>
2023-08-06 20:24:32 +0400

    test_project.py

--- 1 ეტაპი/project/test_project.py
+++ 1 ეტაპი/project/test_project.py
@@ -1,4 +1,19 @@
 from project import userChoice, checkChoice, botChoice, finalAnswer
 
+def test_botChoice():
+    assert botChoice() == "rock" or "scissors" or "paper"
 
-print(userChoice("rock"))
+#!!Please use pytest -s for this!!
+def test_userChoice():
+    assert userChoice() == "rock" or "scissors" or "paper"
+
+#!!Please use pytest -s for this!!
+def test_checkChoice():
+    assert checkChoice("") == None
+    assert checkChoice("scissors") == None
+
+#!!Please use pytest -s for this!!
+def test_finalAnswer():
+    assert finalAnswer("rock","paper") == "You lost!"
+    assert finalAnswer("paper","rock") == "You won!"
+    assert finalAnswer("paper","paper") == "Uh-oh! Thats a tie."
